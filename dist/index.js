93d7ce00932f45a5bbcfb13898d8dc2acd33d4b9
sharma-mayankkk <br10216030515@gmail.com>
2026-08-16 15:24:14 +0530

    Add gitignore and remove generated files

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(person) {
-    return `Hello!! ${person}, welcome to Gotham`;
-}
-const username = "Batman";
-console.log(greet(username));
-//# sourceMappingURL=index.js.map
