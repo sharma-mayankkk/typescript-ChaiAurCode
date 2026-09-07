1af4938433f9c05ccea92ae108307a7848951676
sharma-mayankkk <br10216030515@gmail.com>
2026-09-07 11:38:11 -0300

    Generic in TS

---
+++ src/generics.ts
@@ -0,0 +1,36 @@
+function wrapInArray<T>(item: T): T[] {
+    return [item]
+}
+
+wrapInArray('masala')
+wrapInArray(42)
+wrapInArray({ flavour: 'ginger' })
+
+function pair<A, B>(a: A, b: B): [A, B] {
+    return [a, b]
+}
+
+pair('masala', 20)
+pair('masala', { flavour: "Ginger" })
+
+
+//generic interface: 
+
+interface Box<T> {
+    content: T
+}
+
+const numberBox: Box<number> = { content: 10 }
+const stringBox: Box<string> = { content: "S" }
+
+//usecase:
+
+interface ApiPromise<T> {
+    status: number,
+    data: T
+}
+
+const res: ApiPromise<{ flavour: string }> = {
+    status: 200,
+    data: { flavour: "Masala" }
+}
